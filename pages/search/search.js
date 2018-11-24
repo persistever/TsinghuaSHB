208586e144dacd5f3a7108f7f5f42179d1ee2ef2
haoyuLucas <425151683@qq.com>
2018-11-24 17:57:13 +0800

    Add index and search pages

---
+++ pages/search/search.js
@@ -0,0 +1,53 @@
+//index.js
+//获取应用实例
+var WxSearch = require('./wxSearch/wxSearch.js')
+var app = getApp()
+Page({
+  data: {
+    // wxSearchData:{
+    //   view:{
+    //     isShow: true
+    //   }
+    // }
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //初始化的时候渲染wxSearchdata
+    WxSearch.init(that, 43, ['weappdev', '小程序', 'wxParse', 'wxSearch', 'wxNotification']);
+    WxSearch.initMindKeys(['weappdev.com', '微信小程序开发', '微信开发', '微信小程序']);
+  },
+  wxSearchFn: function (e) {
+    var that = this
+    WxSearch.wxSearchAddHisKey(that);
+  },
+  wxSearchInput: function (e) {
+    var that = this
+    WxSearch.wxSearchInput(e, that);
+  },
+  wxSearchFocus: function (e) {
+    var that = this
+    WxSearch.wxSearchFocus(e, that);
+  },
+  wxSearchBlur: function (e) {
+    var that = this
+    WxSearch.wxSearchBlur(e, that);
+  },
+  wxSearchKeyTap: function (e) {
+    var that = this
+    WxSearch.wxSearchKeyTap(e, that);
+  },
+  wxSearchDeleteKey: function (e) {
+    var that = this
+    WxSearch.wxSearchDeleteKey(e, that);
+  },
+  wxSearchDeleteAll: function (e) {
+    var that = this;
+    WxSearch.wxSearchDeleteAll(that);
+  },
+  wxSearchTap: function (e) {
+    console.log("search_tap")
+    var that = this
+    WxSearch.wxSearchHiddenPancel(that);
+  }
+})
