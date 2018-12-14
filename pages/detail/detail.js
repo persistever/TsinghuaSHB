4d5b6b0e2ad260fc5fa880c208a18e5d03d8c319
hanjy <hanjy1997@qq.com>
2018-12-15 01:21:10 +0800

    update detail page

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,38 +1,52 @@
 // pages/detail/detail.js
+var app = getApp()
+
 Page({
   data: {
-    img_url: [
-      '/images/wjf1.jpg',
-      '/images/wjf2.jpg',
-      '/images/wjf3.jpg'
-    ],
-    indicatorDots: false,
-    autoplay: false,
-    interval: 5000,
-    duration: 1000,
-    book_name : '微积分',
-    book_price : '15.9',
+    indicatorDots: true,
+    autoplay: true,
     //书籍信息
-    book_info : '微积分是高等数学中研究函数的微分、积分以及有关概念和应用的数学分支。它是数学的一个基础学科。内容主要包括极限、微分学、积分学及其应用。'
+    img_url: res.data['itemPicturePath'],
+    book_name: res.data['itemName'],
+    book_price: res.data['itemPrice'],
+    book_sort: res.data['itemSort'],
+    book_info: res.data['itemInfo'],
+    book_pub: res.data['itemPublisher'],
+    book_cn: res.data['itemCourseName'],
+    book_ct: res.data['itemCourseTeacher'],
+    useServer: app.globalData.useServer,
+    serverURL: app.globalData.serverURL,
   },
-  changeIndicatorDots: function (e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function (e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange: function (e) {
-    this.setData({
-      duration: e.detail.value
+  onLoad: function () {
+    var that = this
+    /*------------------------------
+     * wx.request()
+     * 说明：请求Page:detail的书籍详情数据
+     * url: serverURL+detail.php
+     * data:{
+     * useServer: bool变量，传给后台表示采用服务器还是本地资源，前端开发无需修改。
+     * }
+     * 请求返回值：res，该变量在successs: function中有效，需要在本页面.data中声明变量接收。
+     -------------------------------*/
+    wx.request({
+      url: that.data.serverURL + "detail.php",
+      data: {
+        useServer: that.data.useServer,
+        serverURL: that.data.serverURL,
+        totalIndex: that.data.totalIndex,
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          list: res.data
+        })
+      },
+      fail: function () {
+        console.log("fail")
+      },
+      complete: function () {
+        // console.log("complete")
+      }
     })
   }
 })
