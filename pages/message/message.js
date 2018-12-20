56292dd7a7f51d81aeb6f61ed989d53bd5530b40
hanjy <hanjy1997@qq.com>
2018-12-21 00:03:22 +0800

    update message

--- pages/message/message.js
+++ pages/message/message.js
@@ -2,10 +2,6 @@
 var app = getApp()
 Page({
   data: {
-    /*
-    messageList: [{ "message": "5块卖吗", "messageTime": "2018-12-15", "messageID": 25, "userIcon": ['../../images/user_icon1.png'], "userName": "社会人", "messageHaveRead": false }, { "message": "skr~skr~~", "messageTime": "2018-12-14", "messageID": 26, "userIcon": ['../../images/user_icon2.jpg'], "userName": "电鳗", "messageHaveRead": false }, { "message": "党的十八大提出，倡导富强、民主、文明、和谐，倡导自由、平等、公正、法治，倡导爱国、敬业、诚信、友善，积极培育和践行社会主义核心价值观。富强、民主、文明、和谐是国家层面的价值目标，自由、平等、公正、法治是社会层面的价值取向，爱国、敬业、诚信、友善是公民个人层面的价值准则，这24个字是社会主义核心价值观的基本内容", "messageTime": "2018-12-18", "messageID": 27, "userIcon": ['../../images/user_icon3.jpeg'], "userName": "温暖の肥宅", "messageHaveRead": false}
-    ]
-    */
     messageList: []
   },
 
@@ -92,5 +88,40 @@ Page({
     })
   },
 
+  chatdel: function(e){
+    var _userid = e.currentTarget.dataset.theotheruserid
+    var _itemid = e.currentTarget.dataset.itemid
+    var that = this
+    wx.showModal({
+      title: '删除',
+      content: '是否删除该聊天',
+      showCancel: true,
+      cancelColor: 'skyblue',
+      confirmColor: 'skyblue',
+      success: function (res) {
+        if (res.cancel) {
+          //点击取消,默认隐藏弹框
+        } 
+        else {
+          var index = 0
+          var temp = that.data.messageList
+          console.log(that.data.messageList)
+          var len = temp.length
+          for(var i=0;i<len;i++){
+            if ((temp[i].itemid == _itemid) && (temp[i].messageTheOtherUserID ==_userid)){
+              index = i
+              break
+            }
+          }
+          that.setData({
+            messageList:temp.splice(index,1)
+          })
+          console.log(index)
+        }
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  }
 })
 
